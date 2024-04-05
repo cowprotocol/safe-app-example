af4f246e419f8292196558c30141efbfc1e3e028
Anxo Rodriguez <anxolin@gmail.com>
2024-04-05 12:13:13 +0200

    Add custom images and sounds

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,22 @@
 import React, { useCallback, useMemo, useState } from 'react'
-import { Paper } from '@mui/material'
 import { useSafeAppsSDK } from '@safe-global/safe-apps-react-sdk'
-import { CowWidget } from './CowWidget'
 import { SafeProvider } from '../SafeProvider'
 import { CowConfig } from './CowConfig'
-import { CowSwapWidgetParams, TradeType } from '@cowprotocol/widget-react'
+import { CowSwapWidget, CowSwapWidgetParams, TradeType } from '@cowprotocol/widget-react'
+import { CowEvents, CowEventListeners } from '@cowprotocol/events'
+
+import { Paper, Chip, Snackbar, Box } from '@mui/material'
 
 const APP_CODE = 'CoW Widget - Safe App'
+
+const BASE_URL = window.location.origin
 
 const DEFAULT_COW_PARAMS: CowSwapWidgetParams = {
   appCode: APP_CODE,
   width: '100%',
   height: '640px',
   standaloneMode: false, // Dapp mode, we handle the Ethereum provider
-  // baseUrl: { environment: 'dev' },
-  // baseUrl: { environment: 'local' },
-  baseUrl: 'https://swap-dev-git-fee-labels-cowswap.vercel.app',
+  baseUrl: 'https://dev.swap.cow.fi',
 
   // tokenLists: ['https://tokens.coingecko.com/uniswap/all.json', 'https://files.cow.fi/tokens/CowSwap.json'],
 
@@ -28,12 +29,12 @@ const DEFAULT_COW_PARAMS: CowSwapWidgetParams = {
     recipient: '0x79063d9173C09887d536924E2F6eADbaBAc099f5',
   },
   images: {
-    emptyOrders: 'https://swap.cow.fi/assets/meditating-cow-v2-AePIJBpI.svg',
+    emptyOrders: BASE_URL + '/images/pig.png',
   },
   sounds: {
-    orderError: null,
-    orderExecuted: null,
-    postOrder: null,
+    orderError: BASE_URL + '/sounds/error.mp3',
+    orderExecuted: BASE_URL + '/sounds/executed.mp3',
+    postOrder: BASE_URL + '/sounds/posted.mp3',
   },
   chainId: 1,
   sell: {
@@ -43,6 +44,20 @@ const DEFAULT_COW_PARAMS: CowSwapWidgetParams = {
   buy: {
     asset: 'COW',
     amount: '0',
+  },
+  theme: {
+    // Configure ypur theme here: https://widget.cow.fi/
+    baseTheme: 'light',
+    primary: '#fa24d7',
+    paper: '#ffe5f6',
+    text: '#560001',
+    // Derived colors. Pick yours or check the derived colors in the widget configuration page (click on "more options")
+    background: '#303030',
+    danger: '#f44336',
+    warning: '#F8D06B',
+    alert: '#DB971E',
+    info: '#428dff',
+    success: '#00D897',
   },
   enabledTradeTypes: [TradeType.SWAP, TradeType.LIMIT, TradeType.ADVANCED],
   tradeType: TradeType.SWAP,
@@ -56,12 +71,57 @@ const SafeApp = (): React.ReactElement => {
     setParams(params)
   }, [])
 
+  const [toasts, setToasts] = useState<String[]>([])
+  const toast = toasts.length > 0 ? toasts[0] : undefined
+
+  const openToast = (message: string) => {
+    setToasts((t) => [...t, message])
+  }
+
+  const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return
+    }
+
+    setToasts((t) => t.slice(1))
+  }
+
+  const listeners = useMemo<CowEventListeners>(() => {
+    return [
+      {
+        event: CowEvents.ON_TOAST_MESSAGE,
+        handler: (event) => {
+          console.info('🍞 New toast message:', event)
+          openToast(event.message)
+        },
+      },
+    ]
+  }, [openToast])
+
   return (
     <Paper elevation={0} sx={{ p: 2 }}>
-      <h1>Your Safe App</h1>
+      <h1>🐷 Your Safe App</h1>
 
       <CowConfig params={params} updateParams={updateWidgetParams} />
-      <CowWidget provider={web3Provider} params={params} />
+
+      <Snackbar
+        open={toasts.length > 0}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message={
+          toast ? (
+            <>
+              <Chip label="YOUR CUSTOM MESSAGES" color="secondary" variant="outlined" />
+              &nbsp;
+              {toast}
+            </>
+          ) : undefined
+        }
+      />
+
+      <Box>
+        <CowSwapWidget params={params} provider={web3Provider} listeners={listeners} />
+      </Box>
     </Paper>
   )
 }
